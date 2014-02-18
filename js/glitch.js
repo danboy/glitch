c4d181e1c39bcaac95bad8748313d788ac3b4b19
Dan Nawara <dan@pinmonkey.com>
2014-02-18 12:00:48 -0600

    adds images and timeout between transitions

--- js/glitch.js
+++ js/glitch.js
@@ -3,10 +3,12 @@ var ImageLoader = function(el, images) {
   this.canvas = document.createElement('canvas');
   this.images = images;
   this.context = this.canvas.getContext('2d');
-  this.animationLength = 1000;
+  this.animationLength = 500;
   this.animationFrames = 10;
   this.currentImageIndex = 0;
+  this.firstRun = true;
   this.reverseImages = false;
+  this.maxTimeout = 20000;
   this.filters = ['pixelate', 'glitch'];
 };
 
@@ -40,7 +42,9 @@ ImageLoader.prototype = {
   },
 
   intervalLength:function(){
-    return this.animationLength/(this.animationFrames+1);
+    var intervalLength = (!this.reverseImages && this.currentImageIndex === 0) ? Math.floor(Math.random()*this.maxTimeout) : this.animationLength/(this.animationFrames+1) ;
+    console.log('timeout',intervalLength);
+    return intervalLength;
   },
 
   createFilteredImages: function(image){
@@ -51,10 +55,16 @@ ImageLoader.prototype = {
       filteredArray[i] = this.filterImage(image, filters);
     };
 
+    if(this.firstRun){
+      this.applyImageToCanvas(filteredArray[0]);
+      this.firstRun = false;
+    }
     this.animateImages(filteredArray);
   },
 
   animateImages: function(images){
+    var intervalLength = this.intervalLength();
+
     if (this.isStopped) {return};
     var i = this.currentImageIndex;
 
@@ -71,7 +81,7 @@ ImageLoader.prototype = {
         this.applyImageToCanvas(images[i]);
         this.currentImageIndex++;
         this.animateImages(images);
-      }.bind(this), this.intervalLength());
+      }.bind(this), intervalLength);
     } else {
       this.currentImageIndex = 0;
       this.drawImage();
@@ -88,6 +98,7 @@ ImageLoader.prototype = {
   }
 };
 
+var images = ['/images/FFF_www_ACOW_BW.jpg','/images/FFF_www_BLACKHEART_BW.jpg','/images/FFF_www_DEESKO_BW.jpg','/images/FFF_www_GUMBALL_BW.jpg','/images/FFF_www_PANZERWOLF_BW.jpg','/images/FFF_www_TOPLESSWYTCH_BW.jpg','/images/FFF_www_ACOW_COL.jpg','/images/FFF_www_BLACKHEART_COL.jpg','/images/FFF_www_DEESKO_COL.jpg','/images/FFF_www_GUMBALL_COL.jpg','/images/FFF_www_PANZERWOLF_COL.jpg','/images/FFF_www_TOPLESSWYTCH_COL.jpg','/images/FFF_www_ALPHAKING_BW.jpg','/images/FFF_www_BLACKSUN_BW.jpg','/images/FFF_www_DREADDOUBLE_BW.jpg','/images/FFF_www_HBI_BW.jpg','/images/FFF_www_POPSKULL_BW.jpg','/images/FFF_www_WARMULLET_BW.jpg','/images/FFF_www_ALPHAKING_COL.jpg','/images/FFF_www_BLACKSUN_COL.jpg','/images/FFF_www_DREADDOUBLE_COL.jpg','/images/FFF_www_HBI_COL.jpg','/images/FFF_www_POPSKULL_COL.jpg','/images/FFF_www_WARMULLET_COL.jpg','/images/FFF_www_ALPHAKONG_BW.jpg','/images/FFF_www_BOOGOOP_BW.jpg','/images/FFF_www_DREADNAUGHT_BW.jpg','/images/FFF_www_JINX_BW.jpg','/images/FFF_www_RABBID_BW.jpg','/images/FFF_www_ZESZESZES_BW.jpg','/images/FFF_www_ALPHAKONG_COL.jpg','/images/FFF_www_BOOGOOP_COL.jpg','/images/FFF_www_DREADNAUGHT_COL.jpg','/images/FFF_www_JINX_COL.jpg','/images/FFF_www_RABBID_COL.jpg','/images/FFF_www_ZESZESZES_COL.jpg','/images/FFF_www_AMONAMARTH_BW.jpg','/images/FFF_www_CALQUEEN_BW.jpg','/images/FFF_www_DRUNKMONK_BW.jpg','/images/FFF_www_LASTSUPPER_BW.jpg','/images/FFF_www_RISGOOP_BW.jpg','/images/FFF_www_ZOMBIEDUST_BW.jpg','/images/FFF_www_AMONAMARTH_COL.jpg','/images/FFF_www_CALQUEEN_COL.jpg','/images/FFF_www_DRUNKMONK_COL.jpg','/images/FFF_www_LASTSUPPER_COL.jpg','/images/FFF_www_RISGOOP_COL.jpg','/images/FFF_www_ZOMBIEDUST_COL.jpg','/images/FFF_www_ANNICA_BW.jpg','/images/FFF_www_CREEPER_BW.jpg','/images/FFF_www_DUSK_BW.jpg','/images/FFF_www_MOLOKO_BW.jpg','/images/FFF_www_RYELIGHTNG_BW.jpg','/images/one.jpg','/images/FFF_www_ANNICA_COL.jpg','/images/FFF_www_CREEPER_COL.jpg','/images/FFF_www_DUSK_COL.jpg','/images/FFF_www_MOLOKO_COL.jpg','/images/FFF_www_RYELIGHTNG_COL.jpg','/images/three.jpg','/images/FFF_www_BACKMASKING_BW.jpg','/images/FFF_www_CSB_BW.jpg','/images/FFF_www_EYEHATE_BW.jpg','/images/FFF_www_MUTINY_BW.jpg','/images/FFF_www_RYETIGER_BW.jpg','/images/two.jpg','/images/FFF_www_BACKMASKING_COL.jpg','/images/FFF_www_CSB_COL.jpg','/images/FFF_www_EYEHATE_COL.jpg','/images/FFF_www_MUTINY_COL.jpg','/images/FFF_www_RYETIGER_COL.jpg','/images/FFF_www_BATTLECHARRO_BW.jpg','/images/FFF_www_DARKLORD_BW.jpg','/images/FFF_www_GORCH_BW.jpg','/images/FFF_www_NELSON_BW.jpg','/images/FFF_www_TIBERIAN_BW.jpg','/images/FFF_www_BATTLECHARRO_COL.jpg','/images/FFF_www_DARKLORD_COL.jpg','/images/FFF_www_GORCH_COL.jpg','/images/FFF_www_NELSON_COL.jpg','/images/FFF_www_TIBERIAN_COL.jpg'];
 
-var myIL = new ImageLoader('.image',['/images/one.jpg','/images/two.jpg','/images/three.jpg']);
+var myIL = new ImageLoader('.image', images);
 myIL.init();
